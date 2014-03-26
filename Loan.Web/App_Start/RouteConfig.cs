4b36574b3cfa744b24d513e04bdf940ee7d0361f
pkwblack <85870344@qq.com>
2014-03-27 00:24:12 +0800

    添加功能页面母版页
    添加功能页面母版页

--- Loan.Web/App_Start/RouteConfig.cs
+++ Loan.Web/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace Loan.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "MainPage", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Loan.Web/Controllers/MainPageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loan.Web.Controllers
+{
+    public class MainPageController : Controller
+    {
+        //
+        // GET: /MainPage/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "青蚨信贷";
+            ViewBag.PageTiTle = "页面标题";
+            ViewBag.PageTiTleDescription = "页面说明";
+            ViewBag.BreadcrumbItem1 = "功能模块名";
+            ViewBag.BreadcrumbItem2 = "功能名称";
+            return View();
+        }
+
+    }
+}
